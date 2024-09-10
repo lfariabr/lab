ca1b569e1d9cc4eed507ed631e26116dbd9d6585
lfariabr <lfariabr@gmail.com>
2024-09-10 18:49:21 +0000

    Updating files

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,15 @@ import streamlit as st
 
 st.title("Hello World!")
 st.write("This is a test")
+
+number1 = st.text_input("Enter first number:")
+number2 = st.text_input("Enter second number:")
+
+if st.button("Calculate"):
+    try:
+        number1 = float(number1)
+        number2 = float(number2)
+        result = number1 + number2
+        st.write(f"The result is: {result}")
+        except ValueError:
+        st.write("Please enter valid numbers.")
