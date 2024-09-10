8ed1988807bd08a152e44bbd95589fb4cdb96329
lfariabr <lfariabr@gmail.com>
2024-09-10 18:54:01 +0000

    Updating files

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ import streamlit as st
 st.title("Hello World!")
 st.write("This is a test")
 
-number1, number2 = None
-
 number1 = st.text_input("Enter first number:")
 number2 = st.text_input("Enter second number:")
 
@@ -15,18 +13,19 @@ result = None
 
 st.button("Calculate")
 
-if operation == "Add":
-    result = float(number1) + float(number2)
-    st.write(f"Result: {result}")
+if number1 and number2:
+  if operation == "Add":
+      result = float(number1) + float(number2)
+      st.write(f"Result: {result}")
 
-elif operation == "Subtract":
-    result = float(number1) - float(number2)
-    st.write(f"Result: {result}")
+  elif operation == "Subtract":
+      result = float(number1) - float(number2)
+      st.write(f"Result: {result}")
 
-elif operation == "Multiply":
-    result = float(number1) * float(number2)
-    st.write(f"Result: {result}")
+  elif operation == "Multiply":
+      result = float(number1) * float(number2)
+      st.write(f"Result: {result}")
 
-elif operation == "Divide":
-    result = float(number1) / float(number2)
-    st.write(f"Result: {result}")
+  elif operation == "Divide":
+      result = float(number1) / float(number2)
+      st.write(f"Result: {result}")
