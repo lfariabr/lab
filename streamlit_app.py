02b7f8aef70d66861b74ae56ade731c22b8e91af
lfariabr <lfariabr@gmail.com>
2024-09-12 15:49:20 +0000

    Updating new files (sidebar)

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,18 @@ if page == "Calculator": # novo
         result = float(number1) / float(number2)
         st.write(f"Result: {result}")
 
+#### NOVO
 elif page == "Currency Converter": # novo
 
   st.title("Currency Converter!")
+
+  real_amount = st.text_input("Enter the amount in real (R$):")
+  exchange_rate = 0.20
+
+  if real_amount:
+    try:
+      real_value = float(real_amount)
+      dollar_value = real_value * exchange_rate
+      st.write(f"Result: ${dollar_value:.2f}")
+    except ValueError:
+      st.write("Invalid input. Please enter a valid number.")
