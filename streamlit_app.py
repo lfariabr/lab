c4be83bd18fc6359ce8fffaa1977e4d118aae62e
lfariabr <lfariabr@gmail.com>
2024-09-12 15:46:21 +0000

    Updating new files (sidebar)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,39 @@
 
 import streamlit as st
 
-st.title("Hello World!")
-st.write("This is a test")
 
-number1 = st.text_input("Enter first number:")
-number2 = st.text_input("Enter second number:")
+# Sidebar for page selection
+st.sidebar.title("Navigation") # Novo
+page = st.sidebar.selectbox("Choose a page", ["Calculator", "Currency Converter"]) # Novo
 
-operation = st.selectbox("Select operation:", ["Add", "Subtract", "Multiply", "Divide"])
+if page == "Calculator": # novo
 
-result = None
+  st.title("Calculator!")
 
-if number1 and number2:
-  if operation == "Add":
-      result = float(number1) + float(number2)
-      st.write(f"Result: {result}")
+  number1 = st.text_input("Enter first number:")
+  number2 = st.text_input("Enter second number:")
 
-  elif operation == "Subtract":
-      result = float(number1) - float(number2)
-      st.write(f"Result: {result}")
+  operation = st.selectbox("Select operation:", ["Add", "Subtract", "Multiply", "Divide"])
 
-  elif operation == "Multiply":
-      result = float(number1) * float(number2)
-      st.write(f"Result: {result}")
+  result = None
 
-  elif operation == "Divide":
-      result = float(number1) / float(number2)
-      st.write(f"Result: {result}")
+  if number1 and number2:
+    if operation == "Add":
+        result = float(number1) + float(number2)
+        st.write(f"Result: {result}")
+
+    elif operation == "Subtract":
+        result = float(number1) - float(number2)
+        st.write(f"Result: {result}")
+
+    elif operation == "Multiply":
+        result = float(number1) * float(number2)
+        st.write(f"Result: {result}")
+
+    elif operation == "Divide":
+        result = float(number1) / float(number2)
+        st.write(f"Result: {result}")
+
+elif page == "Currency Converter": # novo
+
+  st.title("Currency Converter!")
