8555ef6e8a24ed05ee7aa61aac5485a83b14e893
lfariabr <lfariabr@gmail.com>
2024-09-19 17:33:08 +0000

    New updates on 2024-09-19

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,15 +101,15 @@ elif page == "Graphics": # novo
 
   st.write("Número de leads por dia")
 
-  # Create line graph for leads by day
+  # Create line graph for leads by day of the month
   graph_dia_do_mes = px.line(
       groupby_leads_dia_do_mes,
-      x='Dia da entrada',
+      x='Dia do mês',
       y='ID do lead',
-      title='Número de Leads por Dia',
-      labels={'ID do lead': 'Número de Leads', 'Dia da entrada': 'Dia'}
+      title='Número de Leads por Dia do Mês',
+      labels={'ID do lead': 'Número de Leads', 'Dia do mês': 'Dia do Mês'},
+      markers=True  # Adiciona marcadores nos pontos da linha
   )
-
   # Display the graph
   st.plotly_chart(graph_dia_do_mes)
   
