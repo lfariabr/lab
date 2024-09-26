5a255b6f69dcc458a92be2c60313862b9ffe24e1
lfariabr <lfariabr@gmail.com>
2024-09-26 18:26:30 +0000

    New updates on 2024-09-26

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import os
 import requests
 import json
 import streamlit as st
-import pandas as pd # novo 3
+import pandas as pd 
 import plotly.express as px
 
 # Sidebar for page selection
@@ -79,29 +79,31 @@ elif page == "Graphics":
   leads = 'leads.xlsx'
   df_leads = pd.read_excel(leads)
 
-  # Playing with dates
-  df_leads['Dia da entrada'] = pd.to_datetime(df_leads['Dia da entrada']) # trata estes dados como texto
-  df_leads['Dia do mês'] = df_leads['Dia da entrada'].dt.day_name()
+  print(df_leads)
 
-  # Extract day of the month from 'Dia da entrada'
-  df_leads['apenas_o_dia'] = df_leads['Dia da entrada'].dt.day
+  # # Playing with dates
+  # df_leads['Dia da entrada'] = pd.to_datetime(df_leads['Dia da entrada']) # trata estes dados como texto
+  # df_leads['Dia do mês'] = df_leads['Dia da entrada'].dt.day_name()
 
-  # Filter data for SP units
-  df_leads_total = df_leads
+  # # Extract day of the month from 'Dia da entrada'
+  # df_leads['apenas_o_dia'] = df_leads['Dia da entrada'].dt.day
 
-  # Group by dia do mês and count unique leads
-  groupby_leads_dia_do_mes = df_leads.groupby('apenas_o_dia').agg({'ID do lead': 'nunique'}).reset_index()
+  # # Filter data for SP units
+  # df_leads_total = df_leads
 
-  st.write("Número de leads por dia")
+  # # Group by dia do mês and count unique leads
+  # groupby_leads_dia_do_mes = df_leads.groupby('apenas_o_dia').agg({'ID do lead': 'nunique'}).reset_index()
 
-  # Create line graph for leads by day of the month
-  graph_dia_do_mes = px.line(
-      groupby_leads_dia_do_mes,
-      x='apenas_o_dia',
-      y='ID do lead',
-      title='Número de Leads por apenas_o_dia',
-      labels={'ID do lead': 'Número de Leads', 'apenas_o_dia': 'Dia do mês'},
-      markers=True  # Adiciona marcadores nos pontos da linha
-  )
-  # Display the graph
-  st.plotly_chart(graph_dia_do_mes)
+  # st.write("Número de leads por dia")
+
+  # # Create line graph for leads by day of the month
+  # graph_dia_do_mes = px.line(
+  #     groupby_leads_dia_do_mes,
+  #     x='apenas_o_dia',
+  #     y='ID do lead',
+  #     title='Número de Leads por apenas_o_dia',
+  #     labels={'ID do lead': 'Número de Leads', 'apenas_o_dia': 'Dia do mês'},
+  #     markers=True  # Adiciona marcadores nos pontos da linha
+  # )
+  # # Display the graph
+  # st.plotly_chart(graph_dia_do_mes)
