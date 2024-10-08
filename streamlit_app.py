c1a3fa506a2dcea48f0732b06a119de6cfb3c7c8
lfariabr <lfariabr@gmail.com>
2024-10-08 19:05:33 +0000

    New updates on 2024-10-08

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,4 +115,16 @@ elif page == "Graphics":
 elif page == "Forca":
 
   st.title("Forca")
+  import random
+  lista_palavras = []
+
+  # Abrindo o arquivo de palavras
+  with open('content/palavras.txt', 'r') as arquivo:
+      for line in arquivo:
+          lista_palavras.append(line.strip())
+
+  #
+  palavra_secreta = random.choice(lista_palavras)
+  st.write(palavra_secreta)
+
   st.button("TESTE")
