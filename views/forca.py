9b4e7e95d42a8fe6abf2a2e2d5c6a38da4b6ceb2
lfariabr <lfariabr@gmail.com>
2024-10-15 18:55:00 +0000

    New updates on 2024-10-15

--- views/forca.py
+++ views/forca.py
@@ -23,8 +23,11 @@ contador = 0
 if 'palavra_secreta' not in st.session_state:
   st.session_state['palavra_secreta'] = palavra_secreta
 
+  chute = st.text_input("Chute uma letra:", max_chars=1)
+  if st.button("Chutar"):
+    if chute in palavra_secreta:
+      st.write(chute)
+
 if st.button("Mudar palavra"):
   palavra_secreta = random.choice(lista_palavras)
   st.session_state['palavra_secreta'] = palavra_secreta
-
-  st.input("Chute uma letra:")
