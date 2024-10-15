5529cb279bbf32e93e6ab75a550f268832457da2
lfariabr <lfariabr@gmail.com>
2024-10-15 18:49:51 +0000

    New updates on 2024-10-15

--- views/forca.py
+++ views/forca.py
@@ -21,11 +21,11 @@ contador = 0
 
 # Se já inicializei, não será reiniciado novamente
 if 'palavra_secreta' not in st.session_state:
-  st.session_state['contador'] = palavra_secreta
-  
-if st.button("Começar!"):
-  st.session_state['contador'] += 1
+  st.session_state['palavra_secreta'] = palavra_secreta
 
+st.write(st.session_state['palavra_secreta'])
+
+if st.button("Mudar palavra"):
+  palavra_secreta = random.choice(lista_palavras)
+  st.session_state['palavra_secreta'] = palavra_secreta
   st.write(st.session_state['palavra_secreta'])
-
-  st.button("Mudar palavra!"):
