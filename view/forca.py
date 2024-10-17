5773ac8acf86957a11920ae6e653241670199376
lfariabr <lfariabr@gmail.com>
2024-10-17 19:59:23 +0000

    New updates on 2024-10-17

--- view/forca.py
+++ view/forca.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.title("Forca!")
+st.title("Forca")
+  import random
+  lista_palavras = []
+
+  # Abrindo o arquivo de palavras
+  with open('content/palavras.txt', 'r') as arquivo:
+      for line in arquivo:
+          lista_palavras.append(line.strip())
+
+  palavra_secreta = random.choice(lista_palavras)
+  st.write(palavra_secreta)
+
+  st.button("TESTE")
