db378c04c0d3256c7bcaeb5758afd2be6a5d5f9d
lfariabr <lfariabr@gmail.com>
2024-10-17 20:28:28 +0000

    New updates on 2024-10-17

--- views/forca.py
+++ views/forca.py
@@ -24,29 +24,29 @@ if 'palavra_secreta' not in st.session_state:
     # iniciar a palavra de chutes com traços
     palavra_chutada.append("_")    
 
-  st.session_state["palavra_chutada"] = palavra_chutada
+st.session_state["palavra_chutada"] = palavra_chutada
 
-  chute = st.text_input("Chute uma letra:", max_chars=1)
+chute = st.text_input("Chute uma letra:", max_chars=1)
 
-  acertos = 0
-  tentativas = 5
-  acertou = False
+acertos = 0
+tentativas = 5
+acertou = False
 
-  letras_chutadas = []
+letras_chutadas = []
 
-  if st.button("Chutar"):
-    palavra_secreta = st.session_state["palavra_secreta"]
-    palavra_chutada = st.session_state["palavra_chutada"]
+if st.button("Chutar"):
+  palavra_secreta = st.session_state["palavra_secreta"]
+  palavra_chutada = st.session_state["palavra_chutada"]
 
-    for index, letra in enumerate(palavra_secreta):
-      if chute == letra:
-        palavra_chutada[index] = chute
-        acertos += 1
-    
-    palavra_chutada_print = " ".join(palavra_chutada)
-    st.write(palavra_chutada_print)
+  for index, letra in enumerate(palavra_secreta):
+    if chute == letra:
+      palavra_chutada[index] = chute
+      acertos += 1
+  
+  palavra_chutada_print = " ".join(palavra_chutada)
+  st.write(palavra_chutada_print)
 
-        
+
 
 
     # Inserir Loop do Jogo
