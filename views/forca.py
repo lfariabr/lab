c466de672e00fa2d760fec9f71610d95cd3ab88c
lfariabr <lfariabr@gmail.com>
2024-10-17 17:38:39 +0000

    New updates on 2024-10-17

--- views/forca.py
+++ views/forca.py
@@ -12,22 +12,30 @@ with open('content/palavras.txt', 'r') as arquivo:
     for line in arquivo:
         lista_palavras.append(line.strip())
 
-#
+# Creates a random list
 palavra_secreta = random.choice(lista_palavras)
 st.write(palavra_secreta)
 
-# If click
-contador = 0
+for letra in palavra_secreta:
+  st.write(letra)
 
 # Se já inicializei, não será reiniciado novamente
 if 'palavra_secreta' not in st.session_state:
   st.session_state['palavra_secreta'] = palavra_secreta
 
 chute = st.text_input("Chute uma letra:", max_chars=1)
+
+acertos = 0
+tentativas = 5
+acertou = False
+
+letras_chutadas = []
+
 if st.button("Chutar"):
   if chute in palavra_secreta:
     st.write(chute)
+    # Inserir Loop do Jogo
 
-if st.button("Mudar palavra"):
-  palavra_secreta = random.choice(lista_palavras)
-  st.session_state['palavra_secreta'] = palavra_secreta
+# if st.button("Mudar palavra"):
+#   palavra_secreta = random.choice(lista_palavras)
+#   st.session_state['palavra_secreta'] = palavra_secreta
