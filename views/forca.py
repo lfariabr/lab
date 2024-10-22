7081a399bbbad4b0ff97e649a443f59fe04cab65
lfariabr <lfariabr@gmail.com>
2024-10-22 18:48:29 +0000

    New updates on 2024-10-22

--- views/forca.py
+++ views/forca.py
@@ -12,22 +12,19 @@ with open('content/palavras.txt', 'r') as arquivo:
 
 st.title("Jogo da Forca")
 
-# Se já inicializei, não será reiniciado novamente
+# Inicializar valores no session_state
 if "palavra_secreta" not in st.session_state:
     palavra_secreta = random.choice(lista_palavras)  # Selecionar uma palavra aleatória
     st.session_state["palavra_secreta"] = palavra_secreta
-    st.session_state["letras_chutadas"] = []  # Lista vazia para as letras chutadas
-    st.session_state["tentativas"] = 5  # Iniciar tentativas com 5
-    st.session_state["acertos"] = 0  # Contador de acertos
+    st.session_state["letras_chutadas"] = []  # Inicializar lista vazia de letras chutadas
+    st.session_state["tentativas"] = 5  # Inicializar tentativas
+    st.session_state["acertos"] = 0  # Inicializar acertos
 
-    palavra_chutada = []  # Criar uma lista com os chutes
-
-    for letra in palavra_secreta:
-        palavra_chutada.append("_")  # Iniciar a palavra de chutes com traços
-
+    # Criar a lista de traços para a palavra chutada
+    palavra_chutada = ["_" for letra in palavra_secreta]
     st.session_state["palavra_chutada"] = palavra_chutada
 
-# Exibir a palavra secreta temporariamente (para depuração, pode remover depois)
+# Mostrar a palavra secreta temporariamente (para depuração)
 st.write(st.session_state["palavra_secreta"])
 
 # Entrada para o chute
