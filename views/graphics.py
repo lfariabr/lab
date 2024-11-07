84108d50a5fc29c8b537c30d0977a6b7acd5413a
lfariabr <lfariabr@gmail.com>
2024-11-07 19:10:23 +0000

    New updates on 2024-11-07

--- views/graphics.py
+++ views/graphics.py
@@ -3,20 +3,20 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-#### NOVO _ GRÁFICOS - novo
+# Título
 st.title("10 - Leads")
 
 # Parte 1: Carregando e abrindo o arquivo de leads
 leads = 'leads.xlsx'
 df_leads = pd.read_excel(leads)
 
+# Parte 3: Tratando as datas para uso
+df_leads['Dia da entrada'] = pd.to_datetime(df_leads['Dia da entrada']) # Dizendo que isso é uma data
+df_leads['Dia'] = df_leads['Dia da entrada'].dt.day # Isola o dia da coluna "Dia da Entrada"
+
 # Parte 2: Criando um dataframe e exibindo ele na página
 st.write("Dataframe")
 st.dataframe(df_leads)
-
-# Parte 3: Tratando as datas para uso
-df_leads['Dia da entrada'] = pd.to_datetime(df_leads['Dia da entrada']) # Dizendo que isso é uma data
-df_leads['Dia'] = df_leads['Dia da entrada'].dt.day
 
 # Parte 4: Group by por dia do mês contando os leads
 groupby_leads_por_dia = (
