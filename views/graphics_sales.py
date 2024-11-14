ccb811c527c6af28e266b3ef15592082468310d6
lfariabr <lfariabr@gmail.com>
2024-11-14 19:32:49 +0000

    New updates on 2024-11-14

--- views/graphics_sales.py
+++ views/graphics_sales.py
@@ -10,6 +10,16 @@ st.title("Sales Page")
 sales = 'sales.xlsx'
 df_sales = pd.read_excel(sales)
 
+# Parte 3: Montando um groupby com as vendas por dia
+######## 
+# Parte 3a - Criando uma coluna para "DIA"
+
+# Aqui vamos dizer para o código que este campo é uma data (pd.to_datetime)
+df_sales['Data venda'] = pd.to_datetime(df_sales['Data venda']) 
+
+# Isolando o dia do campo "Data venda"
+df_sales['Dia'] = df_sales['Data venda'].dt.day
+
 # Parte 2: Exibir o dataframe no frontend
 st.write("Mostrando o Dataframe no frontend")
 st.dataframe(df_sales)
