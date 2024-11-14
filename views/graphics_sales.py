563028d6dce59a8d560c46670c9696f2d069bf86
lfariabr <lfariabr@gmail.com>
2024-11-14 19:27:38 +0000

    New updates on 2024-11-14

--- views/graphics_sales.py
+++ views/graphics_sales.py
@@ -5,3 +5,11 @@ import streamlit as st
 
 # Título
 st.title("Sales Page")
+
+# Parte 1: Carregando e abrindo o arquivo de sales
+sales = 'sales.xlsx'
+df_sales = pd.read_excel(sales)
+
+# Parte 2: Exibir o dataframe no frontend
+st.write("Mostrando o Dataframe no frontend")
+st.dataframe(df_sales)
