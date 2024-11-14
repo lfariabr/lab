b486b87d03d7c35c07d6c0ce1f69e27e20b614e3
lfariabr <lfariabr@gmail.com>
2024-11-14 19:38:22 +0000

    New updates on 2024-11-14

--- views/graphics_sales.py
+++ views/graphics_sales.py
@@ -10,9 +10,9 @@ st.title("Sales Page")
 sales = 'sales.xlsx'
 df_sales = pd.read_excel(sales)
 
-# Parte 3: Montando um groupby com as vendas por dia
+# Parte 2: Montando um groupby com as vendas por dia
 ######## 
-# Parte 3a - Criando uma coluna para "DIA"
+# Parte 2a - Criando uma coluna para "DIA"
 
 # Aqui vamos dizer para o código que este campo é uma data (pd.to_datetime)
 df_sales['Data venda'] = pd.to_datetime(df_sales['Data venda']) 
@@ -20,6 +20,17 @@ df_sales['Data venda'] = pd.to_datetime(df_sales['Data venda'])
 # Isolando o dia do campo "Data venda"
 df_sales['Dia'] = df_sales['Data venda'].dt.day
 
-# Parte 2: Exibir o dataframe no frontend
+# Parte 3: Exibir o dataframe no frontend
 st.write("Mostrando o Dataframe no frontend")
 st.dataframe(df_sales)
+
+# Parte 4: Group by Vendas por Dia
+groupby_vendas_por_dia = (
+      df_sales
+      .groupby('Dia')                
+      .agg({'Valor líquido': 'sum'})  
+      .reset_index()                   
+  )
+
+st.write("Exibindo o groupby_vendas_por_dia")
+st.dataframe(groupby_vendas_por_dia)
